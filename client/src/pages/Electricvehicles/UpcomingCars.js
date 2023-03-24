06f725cb1dc8ddf785e91c22a16f640ad8ac4529
HiteshMalla1343 <mallahitesh1343@gmail.com>
2023-03-25 00:29:05 +0530

    resolved

--- client/src/pages/Electricvehicles/UpcomingCars.js
+++ client/src/pages/Electricvehicles/UpcomingCars.js
@@ -11,12 +11,8 @@ const UpcomingCars = () => {
 	useEffect(() => {
 		const getAllvehicles = async () => {
 		const temp = await fetch("http://localhost:5000/allvehicles/");
-		// console.log('hi')
-		// console.log(temp)
-		console.log('bye')
 		const tempJson = await temp.json();
 		console.log(tempJson)
-		console.log(tempJson.article);
 		setAllvehicles(tempJson);
 		};
 		getAllvehicles();
@@ -64,71 +60,6 @@ const UpcomingCars = () => {
 						</figure>
 						);
 					})}
-					{/* <figure className="effect-sarah">
-						<img className="image__img" src={Nexon} alt="service" />
-						<figcaption>
-							<h2>
-								<span className="newCar">Tata Nexon EV</span>
-							</h2>
-							<p>
-								Available for starting price of Rs 13.99 lakh ,
-								the Nexon EV comes with a range of 312 km on a
-								single charge, fast charging capability,
-								extended battery life and className-leading
-								safety features.
-							</p>
-							<Link to="/ev_cars/tata-nexon">View more</Link>
-						</figcaption>
-					</figure>
-					<figure className="effect-sarah">
-						<img className="image__img" src={E20} alt="products" />
-						<figcaption>
-							<h2>
-								<span className="newCar">
-									Mahindra e2O Plus
-								</span>
-							</h2>
-							<p>
-								Its Starting price is 7.48 lakh which can be
-								affored easyily and extended battery life and
-								className-leading safety features
-							</p>
-							<Link to="/ev_cars/mahindra-e20">View more</Link>
-						</figcaption>
-					</figure>
-					<figure className="effect-sarah">
-						<img className="image__img" src={Volvo} alt="volvo" />
-						<figcaption>
-							<h2>
-								<span className="newCar">
-									Mercedes Benz EQS
-								</span>
-							</h2>
-							<p>
-								With a rumoured global debut on the 15th of this
-								month, the EQS is set to be Mercedes' flagship
-								electric car, with a new build that is set to
-								sustain as the Germans' standard for future EVs.
-							</p>
-							<Link to="/ev_cars/benz-eqs">View more</Link>
-						</figcaption>
-					</figure>
-					<figure className="effect-sarah">
-						<img className="image__img" src={XC40} alt="xc40" />
-						<figcaption>
-							<h2>
-								<span className="newCar">
-									Volvo XC40 recharge
-								</span>
-							</h2>
-							<p>
-								Its Starting price is 7.48 lakh which can be
-								affored easyily and extended battery life and
-								className-leading safety features
-							</p>
-							<Link to="/ev_cars/xc40-recharge">View more</Link>
-						</figcaption>
-					</figure> */}
 				</div>
 				<br />
 				<br />

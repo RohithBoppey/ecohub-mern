78fe998f2ea8548d6a33510186777167446efab6
RohithBoppey <rohithboppey1298@gmail.com>
2023-04-25 02:34:44 +0530

    made changes for admin reply

--- server/redis/redis.js
+++ server/redis/redis.js
@@ -1,25 +1,13 @@
 const redis = require("redis");
-// const client = redis.createClient(process.env.REDIS_PORT);
-// client
-// 	.connect()
-// 	.then(() => {
-// 		console.log("Redis connected and is running!");
-// 	})
-// 	.catch((err) => {
-// 		console.log(err);
-// 	});
 
+const client = redis.createClient({
+	password: "wFB1LEmrLlcpkQYnYb5v7BJf5OiKX3P8",
+	socket: {
+		host: "redis-13787.c10.us-east-1-2.ec2.cloud.redislabs.com",
+		port: 13787,
+	},
+});
 
+client.connect();
 
-	const client = redis.createClient({
-		password: 'cbs4E67wBspSKw5cKo0ldtyRWO2tCJPb',
-		socket: {
-			host: 'redis-16115.c81.us-east-1-2.ec2.cloud.redislabs.com',
-			port: 16115
-		}
-	});
-
-
-	client.connect();
-	
 module.exports = client;
